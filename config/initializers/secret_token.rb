9be40b76ffbf35465a088380f77b1007a1739d77
JShelto <jshelto@sas.upenn.edu>
2013-12-23 16:50:04 -0500

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp3::Application.config.secret_key_base = '32d3dd444d7bfee5a2d8593e33827a1844a77c43100041015a8a8b2698a2c56a9a975d4f19a4ae8a3528ba5334f9bf066e5dc57d39282e67f7e14bd0e48f3049'
